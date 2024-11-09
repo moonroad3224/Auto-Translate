5ded062d398a9b57bc8b0502448df6c87a763460
moonroad3224 <moonload3224@gmail.com>
2024-11-09 09:16:59 +0900

    動作しなかったのでDis7-Assistantからコードを拝借して修正

--- maj.py
+++ maj.py
@@ -1,32 +1,34 @@
-import datetime,os,sys
+import os,dotenv,datetime
 import discord
-from discord import Option
+from discord import app_commands
+from discord.app_commands import describe
+from discord.ext import commands
 import google.generativeai as genai
 import dotenv
 from dotenv import load_dotenv
 load_dotenv()
-
-geminikey = os.environ.get('APIKEY1') #GEMINIのAPIキー
-bottoken = os.environ.get("TOKEN2") #ボットのトークン
-translateChID = 1296696402742611979 #翻訳チャンネルの指定
-
+geminikey = os.environ.get('APIKEY1')
+bottoken = os.environ.get("TOKEN1")
+ChID = 1296696402742611979 #Channnel ID
 genai.configure(api_key=geminikey)
 model = genai.GenerativeModel('gemini-1.5-flash')
-
-client = discord.Bot()
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+tree = app_commands.CommandTree(client)
+dotenv.load_dotenv()
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(name=f"Last on_ready executed:{datetime.datetime.now()}(+9:00 JST)"))
+    await client.change_presence(activity=discord.Game(name=f"Last on_ready executed:{datetime.datetime.now()}(+09:00 JST)"))
     print(f"{datetime.datetime.now()}:on_ready  client.user:{client.user} ")
+    await tree.sync()
 
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
-    elif message.channel.id == translateChID:
+    elif message.channel.id == ChID:
         print(f"{datetime.datetime.now()}:on_message:{message.content}")
         response = model.generate_content(f"次の文章をカジュアルな感じで英語に翻訳してその文章だけ返してください。また日本語にもカジュアルな感じで翻訳してその文章だけ返してください。：{message.content}")
         print(f"response:\ntext={response.text}done={response._done}\niterator={response._iterator}\nerror={response._error} \nusage_metadata:\n{response.usage_metadata}")
@@ -37,5 +39,7 @@ async def on_message(message):
             await message.channel.send(f'The response appears to have failed. \nPlease contact your administrator.\ndone={response._done}\niterator={response._iterator}\nerror={response._error} ')
             print("Error")
         return
+
+
 
 client.run(f"{bottoken}")
--- val.py
+++ val.py
@@ -1,23 +1,28 @@
-import datetime,os
+import os,dotenv,datetime
 import discord
-from discord import Option
+from discord import app_commands
+from discord.app_commands import describe
+from discord.ext import commands
 import google.generativeai as genai
 import dotenv
 from dotenv import load_dotenv
 load_dotenv()
-geminikey = os.environ.get('APIKEY')
+geminikey = os.environ.get('APIKEY1')
 bottoken = os.environ.get("TOKEN1")
-ChID = 267984302373867651 #Channnel ID
+ChID = 1267984302373867651 #Channnel ID
 genai.configure(api_key=geminikey)
 model = genai.GenerativeModel('gemini-1.5-flash')
-client = discord.Bot()
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+tree = app_commands.CommandTree(client)
+dotenv.load_dotenv()
 
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Game(name=f"Last on_ready executed:{datetime.datetime.now()}(+09:00 JST)"))
     print(f"{datetime.datetime.now()}:on_ready  client.user:{client.user} ")
+    await tree.sync()
 
 @client.event
 async def on_message(message):
@@ -34,5 +39,7 @@ async def on_message(message):
             await message.channel.send(f'The response appears to have failed. \nPlease contact your administrator.\ndone={response._done}\niterator={response._iterator}\nerror={response._error} ')
             print("Error")
         return
+
+
 
 client.run(f"{bottoken}")
